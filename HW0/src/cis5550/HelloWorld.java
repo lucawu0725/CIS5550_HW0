3865f719a51f100d9123e4c0b4e03fc8976bc0b2
lucawu <lucawu0725@gmail.com>
2024-08-30 14:35:15 -0400

    change name

--- HW0/src/cis5550/HelloWorld.java
+++ HW0/src/cis5550/HelloWorld.java
@@ -2,6 +2,6 @@ package cis5550;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World Luca Wu");
+        System.out.println("Hello World (Luca Wu)");
     }
 }
